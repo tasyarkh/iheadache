f87535f707a3abfd55102f839aa0aece75809900
Tasyarkh <tasyaramadhinta@gmail.com>
2023-11-12 23:52:39 +0700

    update text

--- main.py
+++ main.py
@@ -17,53 +17,53 @@ def iheadache():
         #diagnosa penyakit & pertanyaan gejalanya
         gejalaSakit = []
 
-        gejala = input("Apakah anda mengalami pusing atau sakit kepala? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami pusing atau sakit kepala? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Pusing")
 
         #gejala migrain
-        gejala = input("Apakah anda mengalami kepala berdenyut-denyut? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami kepala berdenyut-denyut? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Kepala Berdenyut")
 
-        gejala = input("Apakah anda mengalami sensitif terhadap cahaya dan bunyi? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami sensitif terhadap cahaya dan bunyi? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Sensitif Cahaya")
         
-        gejala = input("Apakah anda mengalami pandangan kabur ? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami pandangan kabur ? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Pandangan Kabur")
 
         #gejala hipertensi / kepala tegang
-        gejala = input("Apakah anda mengalami durasi sakit kepala lebih dari 30 menit hingga 7 hari? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami durasi sakit kepala lebih dari 30 menit hingga 7 hari? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Kepala Tegang")
         
-        gejala = input("Apakah anda mengalami rasa mual? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami rasa mual? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Mual")
 
         #gejala sakit kepala belakang / neuragia okspital
-        gejala = input("Apakah anda mengalami sakit kepala bagian belakang berdenyut? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami sakit kepala bagian belakang berdenyut? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Neuragia Okspital")
         
-        gejala = input("Apakah anda mengalami nyeri tajam di leher atau kulit kepala? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami nyeri tajam di leher atau kulit kepala? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Nyeri Leher")
         
         #gejala sinus
-        gejala = input("Apakah anda mengalami rasa nyeri pada bagian wajah? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami rasa nyeri pada bagian wajah? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Nyeri Wajah")
         
-        gejala = input("Apakah anda mengalami rasa sakit pada telinga? (ya/tidak)").lower()
-        if gejala == "ya":
+        gejala = input("Apakah anda mengalami rasa sakit pada telinga? (y/n)").lower()
+        if gejala == "y":
             gejalaSakit.append("Tekanan Telinga")
 
 
         #jika memiliki riwayat penyakit berat
-        riwayatPenyakit = input("Apakah anda memiliki riwayat penyakit berat sebelumnya ? (ya/tidak)").lower
+        riwayatPenyakit = input("Apakah anda memiliki riwayat penyakit berat sebelumnya ? (y/n)").lower()
 
         #hasil dari diagnosa penyakit & mengelola jawaban dari pertanyaan
         hasilDiagnosis = ""
@@ -92,33 +92,33 @@ def iheadache():
             print("1. Kompress air hangat atau dingin")
             print("2. Cari tempat yang tenang untuk istrirahat")
             print("3. Memperbanyak konsumsi air mineral")
-            if riwayatPenyakit == "ya":
+            if riwayatPenyakit == "y":
                 print("- jika ada riwayat penyakit berat sebelumnya silahkan konsuldasikan ke dokter atau datang kerumah sakit untuk evaluasi lebih lanjut🏥")
         elif hasilDiagnosis == "Hipertensi":
             print("Saran dan penanganan pertama : ")
             print("1. Mengurangi konsumsi garam")
             print("2. Istirahat yang cukup")
             print("3. Hindari Stress dan rileks")
-            if riwayatPenyakit == "ya":
+            if riwayatPenyakit == "y":
                 print("- jika ada riwayat penyakit berat sebelumnya silahkan konsuldasikan ke dokter atau datang kerumah sakit untuk evaluasi lebih lanjut🏥")
         elif hasilDiagnosis == "Neuralgia Oksipital":
             print("Saran dan penanganan pertama : ")
             print("1. Istirahat yang cukup 7-8 jam sehari")
             print("2. Hindari konsumsi alkohol & rokok")
             print("3. Melakukan peregangan agar otot leher tidak kaku")
-            if riwayatPenyakit == "ya":
+            if riwayatPenyakit == "y":
                 print("- jika ada riwayat penyakit berat sebelumnya silahkan konsuldasikan ke dokter atau datang kerumah sakit untuk evaluasi lebih lanjut🏥")
         elif hasilDiagnosis == "Sinusitis":
             print("Saran dan penanganan pertama : ")
             print("1. Hirup uap hangat")
             print("2. Perbanyak istirahat dan minum air putih")
             print("3. Lakukan bilas hidung")
-            if riwayatPenyakit == "ya":
+            if riwayatPenyakit == "y":
                 print("- jika ada riwayat penyakit berat sebelumnya silahkan konsuldasikan ke dokter atau datang kerumah sakit untuk evaluasi lebih lanjut🏥")
 
         #pertanyaan lanjutan untuk proses diagnosa
-        prosesDiagnosa = input("\nApakah anda ingin melanjutkan diagnosa lagi ? (ya/tidak)").lower()
-        if prosesDiagnosa != 'ya':
+        prosesDiagnosa = input("\nApakah anda ingin melanjutkan diagnosa lagi ? (y/n)").lower()
+        if prosesDiagnosa != 'y':
             print("👋🏼Terimakasih semoga lekas sembuh👋🏼")
             break
         
